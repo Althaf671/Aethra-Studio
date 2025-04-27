e087c24fed67cd0c4a2290a4f271e33e334a5326
Althaf671 <althafmain@gmail.com>
2025-04-27 14:06:34 +0700

    testing profile page

--- aethra/src/app/profile/page.tsx
+++ aethra/src/app/profile/page.tsx
@@ -1,7 +1,12 @@
 'use client';
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
-export default function Profile() {
+const Profile = async () => {
+const session = await getServerSession(authOptions)
+console.log(session)
+
     return (
-        <div className="text-center text-3xl text-white">Welcome to your Profile</div>
+        <div className="text-center text-3xl mt-30 text-white">Welcome to your Profile{session?.user.name}</div>
     )
 }
