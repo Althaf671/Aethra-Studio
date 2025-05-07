0e571a20da89ab73e477c48cb27600288834746e
Althaf671 <althafmain@gmail.com>
2025-05-07 07:22:34 +0700

    abc

--- aethra/src/app/api/admin/total-users/route.ts
+++ aethra/src/app/api/admin/total-users/route.ts
@@ -1,15 +1,13 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { db } from '@/lib/db'; 
 import { NextResponse } from 'next/server';
+import { db } from '@/lib/db';
 
 /*==== API Handler ===*/
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const totalUsers = await db.user.count(); 
-
-        return NextResponse.json({ totalUsers });
-    } catch (error) {
-        console.error("Error fetching total users:", error);
-        return NextResponse.json({ error: 'Failed to fetch total users' }, { status: 500 });
-    }
+export async function GET(request: Request) {
+  try {
+    const totalUsers = await db.user.count();
+    return NextResponse.json({ totalUsers });
+  } catch (error) {
+    console.error("Error fetching total users:", error);
+    return NextResponse.json({ error: 'Failed to fetch total users' }, { status: 500 });
+  }
 }
